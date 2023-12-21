52dc1f2173f12849a3b6150cad3b2658ccbe9c22
kyomi <me@kyomi.dev>
2023-12-21 12:00:00 +0100

    feat(postgresql): initial connection with PostgreSQL

---
+++ docker/bot/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly cmd="$*"
+
+export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+
+wait-for-it \
+  --host="$POSTGRES_HOST" \
+  --port="$POSTGRES_PORT" \
+  --timeout=90 \
+  --strict
+
+echo "PostgreSQL is up. Continuing..."
+
+# Apply database migrations
+# alembic upgrade head
+
+# Evaluating passed command (do not touch):
+exec $cmd
