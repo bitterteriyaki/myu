32a40946f4abee952b1c61061f272f9ce0a31a4c
kyomi <me@kyomi.dev>
2023-12-24 12:00:00 +0100

    feat(logging): initial rotating log system

--- manage.py
+++ manage.py
@@ -1,8 +1,46 @@
 from os import environ
+from contextlib import contextmanager
+from collections.abc import Generator
+from logging import getLogger, INFO, WARN, Formatter
+from logging.handlers import RotatingFileHandler
 
 from click import group
 
 from bot.core import Myu
+
+
+@contextmanager
+def setup_logging() -> Generator[None, None, None]:
+    log = getLogger()
+
+    try:
+        max_bytes = 32 * 1024 * 1024
+
+        getLogger("discord").setLevel(INFO)
+        getLogger("discord.http").setLevel(WARN)
+
+        log.setLevel(INFO)
+
+        handler = RotatingFileHandler(
+            "logs/myu.log",
+            encoding="utf-8",
+            maxBytes=max_bytes,
+            backupCount=5,
+        )
+
+        datetime_format = "%Y-%m-%d %H:%M:%S"
+        log_format = "[{asctime}] [{levelname}] {name}: {message}"
+
+        formatter = Formatter(log_format, datetime_format, style="{")
+
+        handler.setFormatter(formatter)
+        log.addHandler(handler)
+
+        yield
+    finally:
+        for handler in log.handlers[:]:
+            handler.close()
+            log.removeHandler(handler)
 
 
 @group()
@@ -13,10 +51,11 @@ def main() -> None:
 @main.command()
 def runbot() -> None:
     """Run the bot."""
-    bot = Myu()
-    token = environ["DISCORD_TOKEN"]
+    with setup_logging():
+        bot = Myu()
+        token = environ["DISCORD_TOKEN"]
 
-    bot.run(token, log_handler=None)
+        bot.run(token, log_handler=None)
 
 
 if __name__ == "__main__":
